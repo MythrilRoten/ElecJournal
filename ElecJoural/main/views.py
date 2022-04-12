a5efabe753ba7f85e2e5c4c0c803bab66d9d18a2
MythrilRoten <68283752+MythrilRoten@users.noreply.github.com>
2022-04-13 00:34:26 +0500

    Delete ElecJoural directory

--- ElecJoural/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def func__auth(request):
-    return render(request, 'main/auth.html')
-
-def func__main_content(request):
-    return render(request, 'main/main_content.html')
