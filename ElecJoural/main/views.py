124c6c6841a2d3f4b1128c964785941ef720a12d
Егор Недоспасов <diedhuntif@gmail.com>
2022-04-13 19:27:54 +0500

    Начало статик файлов

--- ElecJoural/main/views.py
+++ ElecJoural/main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+
 
 # Create your views here.
 
